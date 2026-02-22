4b379d6c3a381537591c05aaf61decef00451c60
tugba-koc <koctug@itu.edu.tr>
2026-02-22 17:36:04 +0300

    first commit

---
+++ fitness-ms/userservice/src/main/java/com/fitness/userservice/entities/concretes/User.java
@@ -0,0 +1,55 @@
+package com.fitness.userservice.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CurrentTimestamp;
+
+import com.fitness.userservice.entities.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="User")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        @Column(name="id")
+        private String id;
+
+        @Column(name="email", nullable = false, unique = true)
+        private String email;
+
+        @Column(name="first_name", nullable = false)
+        private String firstName;
+
+        @Column(name="last_name", nullable = false)
+        private String lastName;
+
+        @Column(name="password", nullable = false)
+        private String password;
+
+        @Enumerated(EnumType.STRING)
+        private UserRole role = UserRole.USER;
+
+        @CurrentTimestamp
+        @Column(name="created_at", nullable = false)
+        private LocalDateTime createdAt;
+
+        @Column(name="updated_at")
+        private LocalDateTime updatedAt;
+}
---
+++ fitness-ms/userservice/src/main/java/com/fitness/userservice/entities/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.fitness.userservice.entities.enums;
+
+public enum UserRole {
+        USER,
+        ADMIN
+}
