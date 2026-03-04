7482a014b58e37fd7ad5707c00af59073e53ae0b
tugba-koc <koctug@itu.edu.tr>
2026-03-05 00:39:12 +0300

    feat: add settings for Java build configuration, update MongoDB dependency, and enhance Recommendation entity with constructors

--- fitness-ms/aiservice/src/main/java/com/fitness/aiservice/entities/concretes/Recommendation.java
+++ fitness-ms/aiservice/src/main/java/com/fitness/aiservice/entities/concretes/Recommendation.java
@@ -7,11 +7,15 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "recommendations")
 public class Recommendation {
         @Id
--- fitness-ms/aiservice/src/main/java/com/fitness/aiservice/webApi/controllers/RecommendationController.java
+++ fitness-ms/aiservice/src/main/java/com/fitness/aiservice/webApi/controllers/RecommendationController.java
@@ -13,8 +13,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/recommendations")
