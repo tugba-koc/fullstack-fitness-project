fddf4611b40d21f73d6f42d38a3490688df10cd5
tugba-koc <koctug@itu.edu.tr>
2026-03-07 16:25:31 +0300

    feat: add logging capability to ActivityServiceManager

--- fitness-ms/activityservice/src/main/java/com/fitness/activityservice/business/concretes/ActivityServiceManager.java
+++ fitness-ms/activityservice/src/main/java/com/fitness/activityservice/business/concretes/ActivityServiceManager.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityServiceManager implements ActivityService {
     private final ActivityRepository activityRepository;
     private final UserValidateServiceManager userValidateService;
