f124f896c2683e5c7de8046efd2fdf25731aad79
tugba-koc <koctug@itu.edu.tr>
2026-03-09 23:21:17 +0300

    feat: add WebClient configuration for reactive web client support

--- fitness-ms/aiservice/src/main/java/com/fitness/aiservice/business/listener/ActivityMessageListener.java
+++ fitness-ms/aiservice/src/main/java/com/fitness/aiservice/business/listener/ActivityMessageListener.java
@@ -2,16 +2,22 @@ package com.fitness.aiservice.business.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import com.fitness.aiservice.business.concretes.ActivityAiManager;
 import com.fitness.aiservice.entities.concretes.Activity;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ActivityMessageListener {
+        private final ActivityAiManager activityAiManager;
+
         @RabbitListener(queues = "activity.queue")
         public void receiveActivityMessage(Activity activity) {
                 log.info("Received activity message: {}", activity);
+                log.info("Generating recommendation for activity {}", activityAiManager.generateRecommendation(activity));
         }
 }
